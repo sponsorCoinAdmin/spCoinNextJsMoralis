70ad230b804d3c192854ac07de79a7c6d1645903
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2023-11-02 18:56:30 -0400

    WIP

---
+++ sponsor-coin/app/components/Header.js
@@ -0,0 +1,41 @@
+'use client';
+import React from "react";
+import Logo from "../data/images/moralis-logo.svg";
+import Eth from "../data/images/eth.svg";
+import { Link } from "react-router-dom";
+
+function Header(props) {
+
+  const {address, isConnected, connect} = props;
+
+  return (
+    <header>
+      <div className="leftH">
+        <img src={Logo} alt="logo" className="logo" />
+        <Link to="/" className="link">
+          <div className="headerItem">Swap</div>
+        </Link>
+        <Link to="/Recipients" className="link">
+          <div className="headerItem">Recipients</div>
+        </Link>
+        <Link to="/Agents" className="link">
+          <div className="headerItem">Agents</div>
+        </Link>
+        <Link to="/Tokens" className="link">
+          <div className="headerItem">Tokens</div>
+        </Link>
+      </div>
+      <div className="rightH">
+        <div className="headerItem">
+          <img src={Eth} alt="eth" className="eth" />
+          Ethereum
+        </div>
+        <div className="connectButton" onClick={connect}>
+          {isConnected ? (address.slice(0,4) +"..." +address.slice(38)) : "Connect"}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
